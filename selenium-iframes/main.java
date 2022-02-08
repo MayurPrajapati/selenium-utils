f56e8417ee4028263af0601c2299763103140def
Mayur Prajapati <mayur@engineer.com>
2022-02-08 10:28:08 +0530

    Update main.java

--- selenium-iframes/main.java
+++ selenium-iframes/main.java
@@ -1,7 +1,15 @@
-			List<Long> indexes = (List<Long>) Global.jse.executeScript("return getIndexOfIframesWithXpath(\""+xpath+"\")");
-			Global.window.switchToDefaultContent();
-			for (int i = 0; i < indexes.size(); i++) {
-				Long index = indexes.get(i);
-				Global.window.switchToFrameUsingXpath("(//*[name()='frame' or name()='iframe' or local-name()='frame' or local-name()='iframe'])[" + index + "]");
-			}
-			Global.button.click(xpath, "Button");
+// "getIndexOfIframesWithXpath()" returns list of indexes of iframes, we can say a route to reach to our expected iframe
+List<Long> indexes = (List<Long>) jse.executeScript("return getIndexOfIframesWithXpath(\""+xpath+"\")");
+// ...
+// code to switch to default content
+// ...
+
+// using this for loop switch to iframes one by one using "indexes"
+for (int i = 0; i < indexes.size(); i++) {
+	Long index = indexes.get(i);
+  // ...
+  // code to switch to iframe using universal xpath
+  // "(//*[name()='frame' or name()='iframe' or local-name()='frame' or local-name()='iframe'])[" + index + "]"
+  // ...
+}
+// here you have access to that element
